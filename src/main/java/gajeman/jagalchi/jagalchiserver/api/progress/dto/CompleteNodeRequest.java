da93f467ff27ab8ba7a506eda354080f11a72c70
heodongun <heodongun0922@gmail.com>
2025-12-27 05:02:40 +0900

    feat(progress): CompleteNodeRequest DTO 추가 refs #18

---
+++ src/main/java/gajeman/jagalchi/jagalchiserver/api/progress/dto/CompleteNodeRequest.java
@@ -0,0 +1,13 @@
+package gajeman.jagalchi.jagalchiserver.api.progress.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CompleteNodeRequest {
+
+    private Boolean isCompleted;
+}
