3b25cc4ae659300bdfedf02a734be4e7e5acd44c
heodongun <heodongun0922@gmail.com>
2025-12-27 04:59:00 +0900

    feat(roadmap): 로드맵 삭제 서비스 추가 refs #17

--- src/main/java/gajeman/jagalchi/jagalchiserver/application/roadmap/RoadmapService.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/application/roadmap/RoadmapService.java
@@ -1,6 +1,8 @@
 package gajeman.jagalchi.jagalchiserver.application.roadmap;
 
+import gajeman.jagalchi.jagalchiserver.common.exception.ResourceNotFoundException;
 import gajeman.jagalchi.jagalchiserver.domain.directory.DirectoryRepository;
+import gajeman.jagalchi.jagalchiserver.domain.roadmap.Roadmap;
 import gajeman.jagalchi.jagalchiserver.domain.roadmap.RoadmapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,4 +15,19 @@ public class RoadmapService {
 
     private final RoadmapRepository roadmapRepository;
     private final DirectoryRepository directoryRepository;
+
+    @Transactional
+    public void delete(Long roadmapId, Long userId) {
+        Roadmap roadmap = findByIdAndOwner(roadmapId, userId);
+        if (roadmap == null) {
+            throw new ResourceNotFoundException("Roadmap", roadmapId);
+        }
+        roadmapRepository.delete(roadmap);
+    }
+
+    private Roadmap findByIdAndOwner(Long roadmapId, Long ownerId) {
+        return roadmapRepository.findById(roadmapId)
+                .filter(roadmap -> roadmap.isOwnedBy(ownerId))
+                .orElse(null);
+    }
 }
