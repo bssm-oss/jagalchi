3f410f00a74a75daf619b2b653416cc35d9fd9a2
heodongun <heodongun0922@gmail.com>
2025-12-27 04:56:56 +0900

    feat(roadmap): 로드맵 수정 서비스 추가 refs #16

--- src/main/java/gajeman/jagalchi/jagalchiserver/application/roadmap/RoadmapService.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/application/roadmap/RoadmapService.java
@@ -1,6 +1,10 @@
 package gajeman.jagalchi.jagalchiserver.application.roadmap;
 
+import gajeman.jagalchi.jagalchiserver.api.roadmap.dto.RoadmapResponse;
+import gajeman.jagalchi.jagalchiserver.api.roadmap.dto.UpdateRoadmapRequest;
+import gajeman.jagalchi.jagalchiserver.common.exception.ResourceNotFoundException;
 import gajeman.jagalchi.jagalchiserver.domain.directory.DirectoryRepository;
+import gajeman.jagalchi.jagalchiserver.domain.roadmap.Roadmap;
 import gajeman.jagalchi.jagalchiserver.domain.roadmap.RoadmapRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -13,4 +17,32 @@ public class RoadmapService {
 
     private final RoadmapRepository roadmapRepository;
     private final DirectoryRepository directoryRepository;
+
+    @Transactional
+    public RoadmapResponse update(Long roadmapId, UpdateRoadmapRequest request, Long userId) {
+        Roadmap roadmap = findByIdAndOwner(roadmapId, userId);
+        if (roadmap == null) {
+            throw new ResourceNotFoundException("Roadmap", roadmapId);
+        }
+
+        if (request.getDirectoryId() != null) {
+            directoryRepository.findById(request.getDirectoryId())
+                    .filter(it -> it.isOwnedBy(userId))
+                    .orElseThrow(() -> new ResourceNotFoundException("Directory", request.getDirectoryId()));
+        }
+
+        roadmap.update(
+                request.getTitle(),
+                request.getDescription(),
+                request.getDirectoryId(),
+                request.getIsPublic());
+
+        return RoadmapResponse.from(roadmap);
+    }
+
+    private Roadmap findByIdAndOwner(Long roadmapId, Long ownerId) {
+        return roadmapRepository.findById(roadmapId)
+                .filter(roadmap -> roadmap.isOwnedBy(ownerId))
+                .orElse(null);
+    }
 }
