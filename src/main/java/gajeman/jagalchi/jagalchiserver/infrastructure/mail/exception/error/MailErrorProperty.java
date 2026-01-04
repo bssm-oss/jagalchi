78c8544afec7f982ba7013300f67e0d6728e3428
leegunwoo <leegunwoo0325@gmail.com>
2026-01-04 22:49:38 +0900

    feat(#1) : 예외 적용

--- src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/mail/exception/error/MailErrorProperty.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/infrastructure/mail/exception/error/MailErrorProperty.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum MailErrorProperty implements ErrorProperty {
-    CANT_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "메일 전송에 실패하였습니다.");
+    CANT_SEND_EMAIL(HttpStatus.INTERNAL_SERVER_ERROR, "서버 측 오류로 메일 전송에 실패했습니다");
 
     private final HttpStatus status;
     private final String message;
