410bf713c789f3900324d3461a4868fdaea80813
이건우 <162273778+leegunwoooo@users.noreply.github.com>
2026-04-14 12:11:04 +0900

    주소추가

--- src/main/java/gajeman/jagalchi/jagalchiserver/global/config/WebConfig.java
+++ src/main/java/gajeman/jagalchi/jagalchiserver/global/config/WebConfig.java
@@ -14,7 +14,8 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
-                        "http://localhost:3000"
+                        "http://localhost:3000",
+                        "https://jagalchi.dev"
                 )
                 .allowedMethods(
                         HttpMethod.GET.name(),
